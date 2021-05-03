a122bc5652cfd4218798f1900fdbfdbd5da1f2a3
Hien Tam Pham <hientam.pham03@gmail.com>
2021-05-03 17:05:24 -0500

    Added API Calls for searched butts and random butts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import Searchbar from './searchbar';
+import { useState } from "react";
 
 
 function App() {
-  // Declare a new state variable, which we'll call "count"
+  const [Pokemon_search, setPokemon_search] = useState('butt');
+  const [pokemonData, setPokemonData] = useState();
+  const [PokemonSpeciesData, setPokemonSpeciesData] = useState ();
+  const [loadingPokemon, setLoadingPokemon] = useState(false);
+  const getPokemonData = (random = false) => {let searchTerm; 
+    if (random) {searchTerm = Math.floor(Math.random() * 898 + 1);} else {searchTerm = Pokemon_search.toLowerCase();}
+    fetch(`https://pokeapi.co/api/v2/pokemon-form/${searchTerm}/`)
+  .then((res) => res.json())
+  .then((result) => {
+    setLoadingPokemon(false);
+    setPokemonData(result);
+
+    getPokemonSpeciesData(searchTerm)
+  }
+)
+const getPokemonSpeciesData = (searchTerm) => {
+  setLoadingPokemon(true);
+
+  fetch(`https://pokeapi.co/api/v2/pokemon-species/${searchTerm}/`)
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        setLoadingPokemon(false);
+        setPokemonSpeciesData(result);
+      })
+
+}}
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +45,12 @@ function App() {
           <h1>Butt Pokedex</h1>
         </a>
         
-        <Searchbar></Searchbar>
-        
+        <Searchbar setPokemon_search={setPokemon_search} Pokemon_search={Pokemon_search} getPokemonData={getPokemonData}/>
+        <p>Number: {pokemonData?.id}</p>
+        <img src={pokemonData?.sprites?.back_default} alt="logo"/>
+        {<ul>{PokemonSpeciesData?.names?.map((nameEntry) => {return (  <li key={nameEntry.language.name}>
+                      {nameEntry.language.name} : {nameEntry.name}
+                    </li>);})}</ul>}
       </header>
       
     </div>
--- my-app/src/searchbar.js
+++ my-app/src/searchbar.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
-function Searchbar() {
-  const [Pokemon_search, setPokemon_search] = useState('butt');
+function Searchbar({setPokemon_search, Pokemon_search, getPokemonData}) {
   return (
     <div className="App">
        
         <input onChange = {(e) => setPokemon_search(e.target.value)}/>
         <p>{Pokemon_search}</p>
+        <button onClick={() => getPokemonData()}>SEARCH BUTT</button>
+        <button onClick={() => getPokemonData(true)}>SURPRISE BUTTS</button>
+
+
     </div>
+    
   );
 }
 
